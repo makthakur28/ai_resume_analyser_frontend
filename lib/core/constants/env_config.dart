584c5aac137e0cc2b257ff9a766897f79bf4e368
Mayank Thakur <mayank@myrevue.app>
2026-05-21 00:16:28 +0530

    added url in envconfig

--- lib/core/constants/env_config.dart
+++ lib/core/constants/env_config.dart
@@ -1,6 +1,6 @@
 class EnvConfig {
   static const String backendUrl = String.fromEnvironment(
     'BACKEND_URL',
-    defaultValue: 'http://localhost:8000',
+    defaultValue: 'https://ai-resume-analyser-backend-cfrr.onrender.com',
   );
 }
